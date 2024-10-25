6a109386a27f1e4e2372e002c10525fafe822f43
LudGalvao <ludgalvao09@gmail.com>
2024-10-25 14:43:59 -0300

    service

--- src/services/arquivo.service.ts
+++ src/services/arquivo.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
+import { CreateArquivoDto } from "src/dto/create-arquivo.dto";
 import { Arquivo } from "src/schemas/arquivo.schema";
 
 
@@ -9,7 +10,7 @@ export class ArquivoService{
 
     constructor(@InjectModel(Arquivo.name) private arquivoModel: Model<Arquivo>){}
 
-    async create(data: any): Promise<Arquivo> {
+    async create(data: CreateArquivoDto): Promise<Arquivo> {
         const criarArquivo = new this.arquivoModel(data);
         return criarArquivo.save();
     }
